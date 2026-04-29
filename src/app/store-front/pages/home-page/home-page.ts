52df5956a3f3c2ffef30d2da035ebf2c083d9e43
Marcus Erick <marcushp1@hotmail.com>
2026-04-28 23:47:36 -0600

    set environments

---
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -0,0 +1,35 @@
+import { Component, effect, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductsService } from '@products/services/products.service';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCard],
+  templateUrl: './home-page.html',
+})
+export class HomePage {
+
+  products: any;
+
+  constructor(){
+    effect( () => {
+      console.log(this.productsResource.value()?.products);
+      this.products = this.productsResource.value()?.products;
+    })
+  }
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        // limit: 1,
+        // offset: 0,
+        // gender: 'women'
+      });
+    },
+  });
+
+}
