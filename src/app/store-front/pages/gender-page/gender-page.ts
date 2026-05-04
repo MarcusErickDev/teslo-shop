a4fa17507f0fd4e3a1768e7eaf807c89280b2bee
Marcus Erick <marcushp1@hotmail.com>
2026-05-03 22:39:05 -0600

    gender page

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
-export class GenderPage { }
+export class GenderPage {
+
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  gender = toSignal(
+    this.route.params.pipe(
+      map(({ gender }) => gender)
+    )
+  );
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        gender: request.gender,
+      });
+    },
+  });
+
+}
